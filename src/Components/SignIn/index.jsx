953d5b6aa14862f936abb105a1bcc518704d196a
saiteja <saitejachicky@gmail.com>
2024-06-19 13:18:29 +0530

    error-handling

--- src/Components/SignIn/index.jsx
+++ src/Components/SignIn/index.jsx
@@ -1,30 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import LoginConfig from "../../JsonConfig/LoginConfig.json";
 import { renderComponentBasedOntype } from "../../Utilis";
+import { validateField } from "../../Utilis";
+
+const initialFormData = LoginConfig?.loginPage?.components.reduce(
+  (acc, element) => {
+    if (element.type === "textField" || element.type === "passwordField") {
+      acc[element.id] = "";
+    }
+    return acc;
+  },
+  {}
+);
 
 const SignIn = () => {
   const navigate = useNavigate();
 
+  const [values, setValues] = useState(initialFormData);
+  const [errors, setErrors] = useState({});
+
+  const handleValueChange = (e) => {
+    const { id, value } = e.target;
+    setValues((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [id]: "",
+    }));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const newErrors = {};
 
-    navigate("/homepage");
+    LoginConfig?.loginPage?.components.forEach((component) => {
+      if (component.validation) {
+        const errorMessage = validateField(
+          component.id,
+          values[component.id],
+          component.validation
+        );
+        if (errorMessage) {
+          newErrors[component.id] = errorMessage;
+        }
+      }
+    });
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      navigate("/homepage");
+    }
   };
-  return (
-    <div className="flex justify-center items-center  flex-col h-screen gap-1 w-full">
-      <h1 className="font-semibold text-lg">SIGN IN</h1>
 
-      <div className="md:w-[70%] lg:w-[40%]   flex flex-col w-[90%]">
-        {LoginConfig?.loginPage?.components.map((component, index) =>
-      (
-        <React.Fragment key={index}>
-          {renderComponentBasedOntype(component, handleSubmit)}
-        </React.Fragment>
-      )
-        )}
-      </div>
+  return (
+    <div className="flex justify-center items-center flex-col h-screen gap-1 w-full">
+      <h1 className="font-bold text-2xl ">SIGN IN</h1>
+      <form
+        className="md:w-[70%] lg:w-[40%] flex flex-col w-[90%]"
+        onSubmit={handleSubmit}
+      >
+        {LoginConfig?.loginPage?.components.map((component, index) => (
+          <React.Fragment key={index}>
+            {renderComponentBasedOntype(
+              component,
+              handleSubmit,
+              handleValueChange
+            )}
+            {errors[component.id] && (
+              <p className="text-red-600 font-semibold">
+                {errors[component.id]}
+              </p>
+            )}
+          </React.Fragment>
+        ))}
+      </form>
     </div>
   );
 };
--- src/Utilis/index.js
+++ src/Utilis/index.js
@@ -1,15 +1,29 @@
-export const renderComponentBasedOntype = (component,handleSubmit) => {
+export const renderComponentBasedOntype = (component,handleSubmit,handlechange) => {
     switch (component.type) {
       case 'label':
         return <label htmlFor={component?.for}  className={component?.className}>{component?.text}</label>;
       case 'textField':
-        return <input type="text" id={component?.id} placeholder={component?.placeholder}  className={component?.className}/>;
+        return <input type="text" id={component?.id} placeholder={component?.placeholder}  className={component?.className}  onChange={handlechange}/>;
       case 'passwordField':
-        return <input type="password" id={component?.id} placeholder={component?.placeholder}  className={component?.className} />;
+        return <input type="password" id={component?.id} placeholder={component?.placeholder}  className={component?.className} onChange={handlechange} />;
       case 'button':
         return <button id={component?.id} onClick={handleSubmit}  className={component?.className}>{component?.text}</button>;
       default:
         return null;
     }
   };
+
+  export const validateField = (fieldId, value, validation) => {
+    if (validation.required && !value) {
+      return "This field is required";
+    }
+    if (validation.pattern) {
+      const regex = new RegExp(validation.pattern);
+      if (!regex.test(value)) {
+        return validation.message;
+      }
+    }
+    return "";
+  };
+
 
